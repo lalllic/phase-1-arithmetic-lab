3bcf8e8799d43811c3880235f0bd3254e2158301
Isabella Costabile <costabileisa@gmail.com>
2022-03-14 15:44:14 -0400

    commit tree 5fd68863fe52426b843fc52e733f18ceb74f1be1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// variables
+const num1 = 62;
+const num2 = 1;
+const num3 = 29;
+const num4 = 5;
 
-// Write your code here
+// multiply; should return 62
+const multiply = num1 * num2;
+
+// generates a random number above 0
+const random = Math.floor(Math.random() * 20) + 1;
+
+// modulo; should return 4
+const mod = num3 % num4;
+
+// returns 20 as the highest number in the set
+const max = Math.max(20);
